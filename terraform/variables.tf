64c0e55fdf2a3cb7bd38a3ef6ffc6496594eed42
SravanAmbati <asravan786@gmail.com>
2023-12-20 17:46:52 +0530

    code update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,27 @@
-variable "subnet_id_1" {
-  type = string
-  default = "subnet-ec90408a"
- }
+variable "location" {
+    default = "ap-south-1"
+}
 
- variable "subnet_id_2" {
-  type = string
-  default = "subnet-0a911b04"
- }
+variable "os_name" {
+    default = "ami-09ba48996007c8b50"
+}
 
- variable "cluster_name" {
-  type = string
-  default = "my-eks-cluster"
- }
+variable "key" {
+    default = "rtp-03"
+}
+
+variable "instance-type" {
+    default = "t2.small"
+}
+
+variable "vpc-cidr" {
+    default = "10.10.0.0/16"  
+}
+
+variable "subnet1-cidr" {
+    default = "10.10.1.0/24"
+  
+}
+variable "subent_az" {
+    default =  "ap-south-1a"  
+}
